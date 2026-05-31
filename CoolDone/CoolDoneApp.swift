8bb3baff338ea1dde040a4304e3be50b5508e818
FrancoisSioArnaud <francois.siornaud@gmail.com>
2026-05-31 19:00:31 +0200

    Initial CoolDone Xcode project

---
+++ CoolDone/CoolDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolDoneApp.swift
+//  CoolDone
+//
+//  Created by François Arnaud on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct CoolDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoolDownWidget/CoolDownWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  CoolDownWidgetBundle.swift
+//  CoolDownWidget
+//
+//  Created by François Arnaud on 31/05/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CoolDownWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CoolDownWidget()
+        CoolDownWidgetControl()
+    }
+}
